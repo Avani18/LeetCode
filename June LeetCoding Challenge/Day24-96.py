73d0f27513efd2dca7d8474a8361cdedba350aa7
Avani Jindal <33371063+Avani18@users.noreply.github.com>
2020-06-25 03:28:15 +0530

    Add files via upload

---
+++ June LeetCoding Challenge/Day24-96.py
@@ -0,0 +1,29 @@
+# Unique Binary Search Trees
+# https://leetcode.com/problems/unique-binary-search-trees/
+'''Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?'''
+
+
+class Solution:
+    def numTrees(self, n: int) -> int:
+        memo={}
+        return self.sol(n,memo)
+     
+    def sol(self, n, memo):
+        if(n not in memo):
+            if(n==1 or n==0): return 1
+            if(n==2): return 2
+            if(n==3): return 5
+            result=0
+            mid=n//2
+            if(n%2==0):
+                for i in range(1,mid+1):
+                    result += 2*self.sol(i-1,memo)*self.sol(n-i,memo)
+            else:
+                mid+=1
+                for i in range(1,mid+1):
+                    if(i!=mid):
+                        result += 2*self.sol(i-1,memo)*self.sol(n-i,memo)
+                    else:
+                        result += self.sol(i-1,memo)*self.sol(n-i,memo)
+            memo[n]=result
+        return memo[n]
