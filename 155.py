96da94fbc1862b2d1989f447a6be9f591b34c3f6
Avani Jindal <33371063+Avani18@users.noreply.github.com>
2020-08-10 02:00:37 +0530

    Add files via upload

---
+++ 155.py
@@ -0,0 +1,23 @@
+# Min Stack
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        
+
+    def pop(self) -> None:
+        del self.stack[-1]        
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
